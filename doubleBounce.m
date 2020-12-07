413ee96612e00ed3bdbe208f1ca59b7322c383d3
VanMyers <vanmmyers@gmail.com>
2020-12-07 03:53:12 -0500

    hacky does not actually work

--- doubleBounce.m
+++ doubleBounce.m
@@ -1,7 +1,7 @@
 function [T, M] = doubleBounce
 
 % Model Parameters
-k = 500;                    % spring constant (N/m) *PLACEHOLDER*
+k = 50000;                 % spring constant (N/m) *PLACEHOLDER*
 cd = 1.2;                   % coefficient of drag (unitless)
 rho = 1.2;                  % density of air at STP (kg/m^3)
 area = .07;                 % area of standing human (m^2) *imprecise*
@@ -11,10 +11,10 @@ mass = 75;                  % weight of person (kg)
 g = 9.8;                    % acceleration of gravity (m/s^2)
 
 % Initial conditions
-init = [0, 0, 0];     % [y, v, x] 
+init = [2, 0, 0];     % [y, v, x] 
 
 % Time
-duration = 2;               % simulation length (s)
+duration = 2;              % simulation length (s)
 framerate = 30;             % animation framerate (s^-1)
 timestep = 1/(framerate*10);
 tspan = [0:timestep:duration];
@@ -25,14 +25,35 @@ options = odeset('Events', @event_func);
 % Calculate the people's positions
 [T, M, te, ye, ie] = ode45(@rate_func, tspan, init, options);
 
-    function res = rate_func(~, X) 
+    function res = rate_func(~, X)
+        % extract state
         y = X(1);
         v = X(2);
-        aDrag = drag(v) / mass;
-        a = aDrag - g;
-        res = [v; a; 0];
+        x = X(3);
+        
+        fDrag = drag(v);
+        fSpring = spring(x);
+        fDown = fDrag - g*mass;
+
+        % don't stick to the spring
+        if y-x <= .00001
+            fOnSpring = fDown;
+            s = fSpring;
+            vSpring = (y-x)/timestep;
+        else
+            fOnSpring = 0;
+            s = 0;
+            vSpring = (0-x)/timestep;
+        end
+        fSpring - fOnSpring;
+        a = (fDown + s) / mass;
+        res = [v; a; vSpring];
     end
 
+    function res = spring(x)
+        res = -k * x;
+    end
+    
     function res = drag(v)
         % Approximate drag force.  Human body shape is too complex.
         res = 0.5 * rho * v^2 * cd * area;
