f0482232ae6783d5be2b9f9dc6dc4900339bea94
VanMyers <vanmmyers@gmail.com>
2020-12-06 23:36:13 -0500

    drag

--- doubleBounce.m
+++ doubleBounce.m
@@ -1,13 +1,16 @@
 function [T, M] = doubleBounce(numBounces)
 
 % Model Parameters
-k = 500;                  % spring constant (N/m) *PLACEHOLDER*
+k = 500;                    % spring constant (N/m) *PLACEHOLDER*
+cd = 1.2;                   % coefficient of drag (unitless)
+rho = 1.2;                  % density of air at STP (kg/m^3)
+area = .07;                 % area of standing human (m^2) *imprecise*
 
 % Physical Constants
-g = 9.8;                  % acceleration of gravity (m/s^2)
+g = 9.8;                    % acceleration of gravity (m/s^2)
 
 % Initial conditions
-init = [0, 0, 0, 0, 0]; % [y(bee), vbee, yber, vber, xspring] 
+init = [0, 0, 0, 0, 0];     % [y(bee), vbee, yber, vber, xspring] 
 
 % Time
 
@@ -19,6 +22,11 @@ options = odeset('Events', @event_func);
 
     function res = rate_func(~, X) 
         
+    end
+
+    function res = drag_force(v)
+        % Approximate drag force.  Human body shape is too complex.
+        res = - 0.5 * rho * v^2 * C_d * area;
     end
 
     % Stop simulation when we aren't bouncing much.
